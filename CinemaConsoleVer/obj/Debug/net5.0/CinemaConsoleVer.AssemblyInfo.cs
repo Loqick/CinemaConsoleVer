cbfc2738bbed6015b200204dfa0d6d471f3d7ac9
Nazar <133972657+QuinqueQ@users.noreply.github.com>
2024-05-23 20:20:03 +0000

    q
    q

--- CinemaConsoleVer/obj/Debug/net5.0/CinemaConsoleVer.AssemblyInfo.cs
+++ CinemaConsoleVer/obj/Debug/net5.0/CinemaConsoleVer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaConsoleVer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4352b88f80e82827dd1b43253670c58dcfe7d57")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaConsoleVer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaConsoleVer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
